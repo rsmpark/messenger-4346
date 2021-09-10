b5c642fb1ce65ea0f56553e4b9a072f0d15ea5bd
rsmpark <smpark95@mindslab.ai>
2021-09-10 15:58:45 -0400

    Add updateMessageRead to Chat click handler.

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,31 +1,33 @@
-import React from "react";
-import { Box } from "@material-ui/core";
-import { BadgeAvatar, ChatContent } from "../Sidebar";
-import { makeStyles } from "@material-ui/core/styles";
-import { setActiveChat } from "../../store/activeConversation";
-import { connect } from "react-redux";
+import React from 'react';
+import { Box } from '@material-ui/core';
+import { BadgeAvatar, ChatContent } from '../Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+import { setActiveChat } from '../../store/activeConversation';
+import { updateMessagesRead } from '../../store/utils/thunkCreators';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     borderRadius: 8,
     height: 80,
-    boxShadow: "0 2px 10px 0 rgba(88,133,196,0.05)",
+    boxShadow: '0 2px 10px 0 rgba(88,133,196,0.05)',
     marginBottom: 10,
-    display: "flex",
-    alignItems: "center",
-    "&:hover": {
-      cursor: "grab"
-    }
-  }
+    display: 'flex',
+    alignItems: 'center',
+    '&:hover': {
+      cursor: 'grab',
+    },
+  },
 }));
 
 const Chat = (props) => {
   const classes = useStyles();
-  const { conversation } = props;
+  const { conversation, user } = props;
   const { otherUser } = conversation;
 
   const handleClick = async (conversation) => {
     await props.setActiveChat(conversation.otherUser.username);
+    await props.updateMessagesRead(user.id, conversation.id);
   };
 
   return (
@@ -45,7 +47,10 @@ const mapDispatchToProps = (dispatch) => {
   return {
     setActiveChat: (id) => {
       dispatch(setActiveChat(id));
-    }
+    },
+    updateMessagesRead: (userId, convoId) => {
+      dispatch(updateMessagesRead(userId, convoId));
+    },
   };
 };
 
