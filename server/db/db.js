b495129de714e7b0b3deb147026034b9794270b1
rsmpark <smpark95@mindslab.ai>
2021-09-13 15:08:27 -0400

    Restore default db config.

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'messenger', 'postgres', 'park-sang', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/messenger', {
   logging: false,
-  host: 'localhost',
-  dialect: 'postgres',
 });
 
 module.exports = db;
